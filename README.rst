b174a9668c30f0848627fc97276056fc82fcaad7
Valerio Belli <belli.valerio@gmail.com>
2018-10-08 22:34:16 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,23 @@
+======
+TOMLer
+======
+
+A simple reader for configuration file with TOML syntax.
+
+Usage
+-----
+This modules implements the Singleton pattern. Hence, after the first call of the class,
+the the module can be accessed simply recalling it.
+
+At first, import the module as follows
+.. code-block::
+
+    from tomler import Config
+
+After that, call for the first time the configuration file as follows
+.. code-block::
+
+    config = Config('<path to the configuration file>', [True | False])
+
+The second parameter is optional. It activates (or deactivates) the hot
+reloading of the configuration file each time a value is accessed.
---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'tomler'
+DESCRIPTION = 'A simple reader for TOML configuration files.'
+URL = 'https://github.com/DrugoLebowski/tomler'
+EMAIL = 'belli.valerio@gmail.com'
+AUTHOR = 'Valerio Belli'
+REQUIRES_PYTHON = '>=3.5.5'
+VERSION = 0.1
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'singleton-decorator',
+    'toml',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/reStructuredText',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
