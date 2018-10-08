6f290e50dcaa428ad0d123d2ea6073d83180e049
Valerio Belli <belli.valerio@gmail.com>
2018-10-09 00:43:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ URL = 'https://github.com/DrugoLebowski/tomler'
 EMAIL = 'belli.valerio@gmail.com'
 AUTHOR = 'Valerio Belli'
 REQUIRES_PYTHON = '>=3.5'
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
